f56efb4d6503518b748197af973243407d49f50f
alfman99 <amanresamntz@gmail.com>
2023-04-06 16:19:58 +0200

    change name httplib.h to httplib.hpp

--- Stub/pch.h
+++ Stub/pch.h
@@ -4,7 +4,7 @@
 #define PCH_H
 
 // Http client
-#include <httplib/httplib.h>
+#include <httplib/httplib.hpp>
 #include <plusaes/plusaes.hpp>
 #include <str_obfuscator/obfuscator.hpp>
 #include <picosha2/picosha2.hpp>
